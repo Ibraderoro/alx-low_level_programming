ee7aeb6937712cca42fd4bc7137155e088d34d5d
Salisu Ibrahim <sanukoli91@gmail.com>
2023-02-28 20:48:51 -0800

    2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,7 +1,9 @@
 #include "main.h"
+
 /**
  * strlen -  a function that returns the length of a string
  * @s: char type pointer
+ *
  * Description: strlen similiar function to return length
  * Return: string
  */
