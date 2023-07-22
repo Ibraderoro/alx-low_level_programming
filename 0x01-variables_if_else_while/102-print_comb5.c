4ca665ccbd2f68ff98033546181dc7f2a8ad902a
Salisu Ibrahim <sanukoli91@gmail.com>
2023-07-22 07:13:14 -0700

    102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0 on exit
+ */
+int main(void)
+
+{
+
+int x;
+int y;
+
+for (x = 0; x < 99; x++)
+	{
+	for (y = x + 1; y < 100; y++)
+	{
+		putchar (x/10 + '0');
+		putchar ((x % 10) + '0');
+		putchar (' ');
+		putchar (y/10 + '0');
+		putchar ((y % 10) + '0');
+
+		if (x == 99 && y == 98)
+		continue;
+
+		putchar (',');
+		putchar (' ');
+	}
+}
+putchar ('\n');
+return (0);
+}
