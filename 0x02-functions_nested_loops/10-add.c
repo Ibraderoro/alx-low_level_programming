f6ccf6591afe4025b3c85ba4f0ca7db7f27e3a73
Salisu Ibrahim <sanukoli91@gmail.com>
2023-07-23 20:23:48 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - This adds two integers
+ * @a: the first number to be computed
+ * @b: the second number to be computed
+ * Return: integer value
+ */
+
+int add(int a, int b)
+{
+
+return (a + b);
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * times_table - This prints the 9 times table
+ * Return: times table
+ */
+
+void times_table(void)
+{
+int x;
+int y;
+int z;
+for (x = 0; x <= 9; x++)
+{
+	_putchar ('0');
+
+	for (y = 1; y < 10; y++)
+	{
+		_putchar (',');
+		_putchar (' ');
+
+		z = x * y;
+
+		if (z <= 9)
+		{
+			_putchar (' ');
+		}
+		else
+		{
+		_putchar ((z / 10) + '0');
+		_putchar ((z % 10) + '0');
+		}
+	}
+_putchar ('\n');
+}
+}
