c193e9c1636b33fcead0775bbc3d8f0230c9010d
Salisu Ibrahim <sanukoli91@gmail.com>
2023-08-05 20:34:50 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,25 +12,26 @@ int y;
 int z;
 for (x = 0; x <= 9; x++)
 {
-	_putchar ('0');
-
-	for (y = 1; y < 10; y++)
+_putchar ('0');
+for (y = 1; y < 10; y++)
 	{
-		_putchar (',');
-		_putchar (' ');
 
-		z = x * y;
-
-		if (z <= 9)
+	_putchar (',');
+	_putchar (' ');
+	
+	z = x * y;
+	if (z <= 9)
 		{
-			_putchar (' ');
+		_putchar (' ');
+		_putchar (z + '0');
 		}
-		else
+	else
 		{
 		_putchar ((z / 10) + '0');
 		_putchar ((z % 10) + '0');
 		}
 	}
+	
 _putchar ('\n');
 }
 }
