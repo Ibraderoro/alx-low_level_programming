a2eed74a1c540e509313224d38e6814256e7e8a5
Salisu Ibrahim <sanukoli91@gmail.com>
2023-08-11 14:30:12 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_numbers - This function prints numbers
+ *
+ * Return: A number
+ */
+void print_numbers(void)
+{
+int x;
+for (x = '0'; x <= '9'; x++)
+{
+_putchar (x);
+}
+_putchar ('\n');
+}
